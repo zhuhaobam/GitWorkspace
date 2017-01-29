f5564dba57899bdb35c5f27479cca0485314a522
zhuhaobam <bamboo123love@163.com>
2017-01-29 18:11:37 +0800

    spring.io奎宿建项目
    目前未写代码，还是一个空项目。

--- README.md
+++
@@ -1,3 +0,0 @@
-# GitWorkspace 
-第一个工作区springboot框架为主要框架的项目。
-准备：1, ssm框架搭建 (springboot + spring framework +  mybatis)【maven or gradle】
---
+++ ssmbam/src/main/java/com/zhuhaobam/mybatis/SsmsosoApplication.java
@@ -0,0 +1,12 @@
+package com.zhuhaobam.mybatis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsmsosoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SsmsosoApplication.class, args);
+	}
+}
